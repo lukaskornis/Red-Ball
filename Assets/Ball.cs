df97d3102d6c478e34ffe3d964d95eb3c3a2a87e
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-11-16 16:41:20 +0200

    Ball movement

---
+++ Assets/Ball.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	public float jumpSpeed = 5;
+	Rigidbody2D rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		var hor = Input.GetAxisRaw("Horizontal");
+		//rb.velocity = new Vector2(hor, 0);
+		rb.AddForce(new Vector2(hor, 0));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			rb.velocity += Vector2.up * jumpSpeed;
+		}
+	}
+}
