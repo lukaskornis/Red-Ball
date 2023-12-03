4e0339ff277f644127e33e0c6c84491e3f9d537d
vincentasa <vincentas.augustas@gmail.com>
2023-12-03 17:31:25 +0200

    added credits and level select scene

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public string sceneName;
+
+    public void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
